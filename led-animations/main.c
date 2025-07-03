0e5901e5ecbac006588882eafafafbaba7164aa0
falb18 <arturo.lopezbo@gmail.com>
2025-07-03 10:04:21 -0600

    Add four LED animations
    For the moment only 4 LEDs are connected to PORTB.

--- led-animations/main.c
+++ led-animations/main.c
@@ -15,14 +15,22 @@
 static __code uint16_t __at (_CONFIG) configword1 = _CP_OFF & _WDT_OFF & _PWRTE_ON & _XT_OSC;
 
 #define TMR0_COUNT 100 // Time overflow set to 5 ms
-#define COUNT_DELAY_LED 100 // 100 * 5ms = 500 ms
+#define COUNT_PAUSE 60          // 60 * 5ms = 300 ms
+#define COUNT_TRANSITION_1 20   // 20 * 5ms = 100 ms
+#define COUNT_TRANSITION_2 50   // 50 * 5ms = 250 ms
+#define REPEAT_ANIMATION_COUNT_1 8
+#define REPEAT_ANIMATION_COUNT_2 4
 
-volatile uint16_t delay_animation = 0;
+volatile uint16_t delay_pause = 0;
+volatile uint16_t delay_transition = 0;
+
+static uint8_t animation_repeats = 0;
 
 static void interrupt_vector(void) __interrupt (0)
 {
     if (INTCONbits.T0IF == 1) { // Check if the interrupt was caused by TMR0
-        delay_animation++;
+        delay_pause++;
+        delay_transition++;
         TMR0 = TMR0_COUNT;      // Reset count for TMR0 register
         INTCONbits.T0IF = 0;    // TMR0 starts counting again
     }
@@ -35,17 +43,91 @@ static void init_TMR0(void)
     INTCON = 0xA0;      // Enable global interrupts (GIE = 1), enable TMR0 interrupt (T0IE = 1)
 }
 
+static void reset_timers(void)
+{
+    delay_pause = 0;
+    delay_transition = 0;
+    animation_repeats = 0;
+}
+
 void main(void)
 {
-    TRISBbits.TRISB0 = 0;    // Pin RB0 as output
-    PORTBbits.RB0 = 0;      // Pin RB0 off
+    uint8_t i = 0;
+    TRISB = 0x00;       // PORTB as output
+    PORTB = 0b00000000; // All pins of PORTB off
 
     init_TMR0();
 
     while (true) {
-        if (delay_animation == COUNT_DELAY_LED) {
-            PORTBbits.RB0 ^= 1; // Change the state of the pin
-            delay_animation = 0;// Reset timer
+        /* ANIMATION 1: blink 4 LEDs and then other 4 LEDs */
+        PORTB = 0b11110000;
+        while (delay_pause < COUNT_PAUSE);
+        reset_timers();
+
+        while (animation_repeats < REPEAT_ANIMATION_COUNT_1) {
+            if (delay_transition == COUNT_TRANSITION_1) {
+                PORTB = ~(PORTB);
+                delay_transition = 0;
+                animation_repeats++;
+            }
         }
+        reset_timers();
+        
+        /* ANIMATION 2: alternate blinking LEDs */
+        PORTB = 0b10101010;
+        while (delay_pause < COUNT_PAUSE);
+        reset_timers();
+
+        while (animation_repeats < REPEAT_ANIMATION_COUNT_1) {
+            if (delay_transition == COUNT_TRANSITION_2) {
+                PORTB = ~(PORTB);
+                delay_transition = 0;
+                animation_repeats++;
+            }
+        }
+        reset_timers();
+
+        /* ANIMATION 3: shift LED ON to the right */
+        PORTB = 0b00000001;
+        i = PORTB;
+        while (delay_pause < COUNT_PAUSE);
+        reset_timers();
+
+        while (animation_repeats < REPEAT_ANIMATION_COUNT_2) {
+            if (delay_transition == COUNT_TRANSITION_1) {
+                i <<= 1;
+                if (i == 16) {
+                    i = 0b00000001;
+                    animation_repeats++;
+                }
+                PORTB = i;
+                delay_transition = 0;
+            }
+        }
+        reset_timers();
+
+        /* ANIMATION 4: shift LED ON to the left */
+        PORTB = 0b00001000;
+        i = PORTB;
+        while (delay_pause < COUNT_PAUSE);
+        reset_timers();
+
+        while (animation_repeats < REPEAT_ANIMATION_COUNT_2) {
+            if (delay_transition == COUNT_TRANSITION_1) {
+                i >>= 1;
+                if (i == 0) {
+                    i = 0b00001000;
+                    animation_repeats++;
+                }
+                PORTB = i;
+                delay_transition = 0;
+            }
+        }
+        reset_timers();
+
+        /* End of animations */
+        PORTB = 0b00000000;
+        while (delay_pause < COUNT_PAUSE);
+        reset_timers();
     }
 }
